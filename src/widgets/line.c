de9ec9186e5e7ecc59d5f2c39c1ee3d8866e2f19
Jakub Filipowicz <jakubf@gmail.com>
2015-11-05 22:13:31 +0100

    add missing source

---
+++ src/widgets/line.c
@@ -0,0 +1,100 @@
+//  Copyright (c) 2015 Jakub Filipowicz <jakubf@gmail.com>
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "tile.h"
+#include "event.h"
+#include "style.h"
+#include "print.h"
+
+struct line {
+	int align;
+};
+
+// -----------------------------------------------------------------------
+void emui_line_draw(struct emui_tile *t)
+{
+	struct line *d = t->priv_data;
+
+	if (d->align == AL_HORIZONTAL) {
+		emuihline(t, 0, 0, t->w, t->style);
+	} else if (d->align == AL_VERTICAL) {
+		emuivline(t, 0, 0, t->h, t->style);
+	}
+}
+
+// -----------------------------------------------------------------------
+void emui_line_debug(struct emui_tile *t)
+{
+}
+
+// -----------------------------------------------------------------------
+int emui_line_update_geometry(struct emui_tile *t)
+{
+	return 0;
+}
+
+// -----------------------------------------------------------------------
+int emui_line_event_handler(struct emui_tile *t, struct emui_event *ev)
+{
+	// event has not been handled
+	return 1;
+}
+
+// -----------------------------------------------------------------------
+void emui_line_destroy_priv_data(struct emui_tile *t)
+{
+	free(t->priv_data);
+}
+
+// -----------------------------------------------------------------------
+struct emui_tile_drv emui_line_drv = {
+	.draw = emui_line_draw,
+	.debug = emui_line_debug,
+	.update_geometry = emui_line_update_geometry,
+	.event_handler = emui_line_event_handler,
+	.destroy_priv_data = emui_line_destroy_priv_data,
+};
+
+// -----------------------------------------------------------------------
+struct emui_tile * emui_line(struct emui_tile *parent, int align, int x, int y, int len)
+{
+	struct emui_tile *t;
+	int w, h;
+
+	if (align == AL_HORIZONTAL) {
+		w = len;
+		h = 1;
+	} else if (align == AL_VERTICAL) {
+		w = 1;
+		h = len;
+	} else {
+		return NULL;
+	}
+
+	t = emui_tile_create(parent, -1, &emui_line_drv, F_WIDGET, x, y, w, h, 0, 0, 0, 0, NULL, P_NONE);
+
+	t->priv_data = calloc(1, sizeof(struct line));
+	struct line *d = t->priv_data;
+	d->align = align;
+
+	return t;
+}
+
+// vim: tabstop=4 shiftwidth=4 autoindent
